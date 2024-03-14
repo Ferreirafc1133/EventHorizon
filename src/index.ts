738ad2f958a0cd8e674306df2f3befa3634b9889
Ferreira <jose.chavez@iteso.mx>
2024-03-13 23:31:47 -0600

    conexion a la bd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
-import routes  from './routes';
-const app = express();
+import routes from './routes';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DB_URL!)
+  .then(() => console.log('Conexión a MongoDB exitosa'))
+  .catch((err: Error) => console.error('Error conectando a MongoDB', err));
 
 app.use(routes);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port =  ${port}`);
+    console.log(`Server running on port ${port}`);
 });
